24143125e0b8b97f363a690df11b67b20577bde9
Michael Celey <michaelc@roadtrippers.com>
2020-03-03 20:30:43 -0500

    Adding dependencies and adding in zip alignment.

--- main.go
+++ main.go
@@ -1,31 +1,98 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"os"
-	"os/exec"
+	"path/filepath"
+	"strings"
+
+	"github.com/bitrise-io/go-steputils/stepconf"
+	"github.com/bitrise-io/go-utils/command"
+	"github.com/bitrise-io/go-utils/log"
+	"github.com/bitrise-tools/go-android/sdk"
+	"github.com/bitrise-tools/go-steputils/tools"
 )
 
+type configs struct {
+	UnalignedAPKPath string `env:"bitrise_unaligned_apk_path,required"`
+}
+
 func main() {
-	fmt.Println("This is the value specified for the input 'example_step_input':", os.Getenv("example_step_input"))
 
-	//
-	// --- Step Outputs: Export Environment Variables for other Steps:
-	// You can export Environment Variables for other Steps with
-	//  envman, which is automatically installed by `bitrise setup`.
-	// A very simple example:
-	cmdLog, err := exec.Command("bitrise", "envman", "add", "--key", "EXAMPLE_STEP_OUTPUT", "--value", "the value you want to share").CombinedOutput()
-	if err != nil {
-		fmt.Printf("Failed to expose output with envman, error: %#v | output: %s", err, cmdLog)
-		os.Exit(1)
+	var cfg configs
+	if err := stepconf.Parse(&cfg); err != nil {
+		failf("Issue with input: %s", err)
 	}
-	// You can find more usage examples on envman's GitHub page
-	//  at: https://github.com/bitrise-io/envman
 
-	//
-	// --- Exit codes:
-	// The exit code of your Step is very important. If you return
-	//  with a 0 exit code `bitrise` will register your Step as "successful".
-	// Any non zero exit code will be registered as "failed" by `bitrise`.
+	androidHome := os.Getenv("ANDROID_HOME")
+	androidSDK, err := sdk.New(androidHome)
+	if err != nil {
+		failf("Failed to create SDK model, error: %s", err)
+	}
+
+	zipalign, err := androidSDK.LatestBuildToolPath("zipalign")
+	if err != nil {
+		failf("Failed to find zipalign path, error: %s", err)
+	}
+
+	unalignedPath := strings.TrimSpace(cfg.UnalignedAPKPath)
+	alignedPath := getAlignedApkName(unalignedPath)
+
+	if err := zipalignApkArtifact(zipalign, unalignedPath, alignedPath); err != nil {
+		failf("Failed to zipalign Build Artifact, error: %s", err)
+	}
+	fmt.Println()
+
+	if err := tools.ExportEnvironmentWithEnvman("BITRISE_ALIGNED_APK_PATH", alignedPath); err != nil {
+		failf("Failed to export APK (%s) error: %s", alignedPath, err)
+	} else {
+		log.Donef("The aligned APK path is now available in the Environment Variable: BITRISE_ALIGNED_APK_PATH (value: %s)", alignedPath)
+	}
+
 	os.Exit(0)
+}
+
+func getAlignedApkName(unalignedApkPath string) string {
+	apkDirectory := filepath.Dir(unalignedApkPath)
+	unalignedApkName := filepath.Base(unalignedApkPath)
+	apkExtension := filepath.Ext(unalignedApkName)
+	alignedApkName := strings.TrimSuffix(unalignedApkName, apkExtension)
+	alignedApkName = fmt.Sprintf("%s-aligned%s", alignedApkName, apkExtension)
+	return filepath.Join(apkDirectory, alignedApkName)
+}
+
+func zipalignApkArtifact(zipalign, inPath, outPath string) error {
+	cmdSlice := []string{zipalign, "-f", "-p", "4", inPath, outPath}
+
+	prinatableCmd := command.PrintableCommandArgs(false, cmdSlice)
+	log.Printf("=> %s", prinatableCmd)
+
+	_, err := executeForOutput(cmdSlice)
+	return err
+}
+
+func failf(format string, v ...interface{}) {
+	log.Errorf(format, v)
+	os.Exit(1)
+}
+
+func executeForOutput(cmdSlice []string) (string, error) {
+	cmd, err := command.NewFromSlice(cmdSlice)
+	if err != nil {
+		return "", fmt.Errorf("Failed to create command, error: %s", err)
+	}
+
+	var outputBuf bytes.Buffer
+	writer := io.MultiWriter(&outputBuf)
+	cmd.SetStderr(writer)
+	cmd.SetStdout(writer)
+
+	err = cmd.Run()
+	if err != nil {
+		err = fmt.Errorf("%s\n%s", outputBuf.String(), err)
+	}
+
+	return outputBuf.String(), err
 }
